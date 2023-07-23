c9d1c95509b1bb8877a61c26c1e809fa33d6bc0f
Olha <o.v.shevel@gmail.com>
2023-07-23 23:28:17 +0300

    4 - arrow function - done

---
+++ 1-javascript-basics/4-arrow-function.js
@@ -0,0 +1,18 @@
+function sumOfSquares(a, b) {
+    return Math.pow(a, 2) + Math.pow(b, 2);
+}
+
+const a = 2;
+const b = 3;
+
+let sum1 = (a, b) => {
+    const result = Math.pow(a, 2) + Math.pow(b, 2);
+    console.log(`${a}^2 + ${b}^2 = ${result}`);
+    return result;
+};
+
+let sum2 = (a, b) => { return `${a}^2 + ${b}^2 = ${Math.pow(a, 2) + Math.pow(b, 2)}`};
+
+sum1(a, b);
+
+console.log(sum2(a, b));
