57d29549e2712a3670df035c3d22dfc9edb2d5fb
Olha <o.v.shevel@gmail.com>
2023-07-24 22:37:40 +0300

    3 - arrays basic - done

---
+++ 2-javascript-basics/3-arrays-basic.js
@@ -0,0 +1,17 @@
+const myList = ['Apple', 'Cat', 'Banana', 'Orange'];
+console.log(myList);
+
+const removedElement = myList.pop()
+console.log(myList);
+
+myList.unshift(removedElement);
+console.log(myList);
+
+myList.shift();
+console.log(myList);
+
+myList.push(removedElement);
+console.log(myList);
+
+
+
