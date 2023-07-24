fec4fe7b98906f4ba97e30dbf457c7505273005b
Olha <o.v.shevel@gmail.com>
2023-07-24 22:50:59 +0300

    5 - slice - done

---
+++ 2-javascript-basics/5-arrays-slice.js
@@ -0,0 +1,8 @@
+const arr = ['Apple', 'Cat', 'Banana', 'Orange'];
+
+const part1 = arr.slice(0, arr.length / 2);
+const part2 = arr.slice(arr.length / 2, arr.length);
+
+console.log(arr);
+console.log(part1);
+console.log(part2);
