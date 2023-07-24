3ed39d6cd541e10bb4b9d5d1a4121ed3b928da9c
Olha <o.v.shevel@gmail.com>
2023-07-24 22:15:00 +0300

    1 - strings - done

---
+++ 2-javascript-basics/1-strings-basic.js
@@ -0,0 +1,15 @@
+const anime = "Totoro";
+console.log(anime.toUpperCase());
+console.log(anime.toLowerCase());
+
+const number = 1.345231;
+console.log(`number = ${number} and type of number: ${typeof(number)}`);
+
+const number2 = Number(number.toFixed(2));
+console.log(`number2 = ${number2} and type of number2: ${typeof(number2)}`);
+
+const shortNumber = 1.5;
+console.log(`short number = ${shortNumber} and type of short number: ${typeof(shortNumber)}`);
+
+const shortNumber2 = Number(shortNumber.toFixed(3));
+console.log(`short number2 = ${shortNumber2} and type of short number2: ${typeof(shortNumber2)}`);
