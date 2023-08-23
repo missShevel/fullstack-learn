4f544d59d475030b4469252302dbe272ec33a5c3
Olha <o.v.shevel@gmail.com>
2023-08-23 13:00:37 +0300

    small fix

--- 9-classes/composer.js
+++ 9-classes/composer.js
@@ -26,11 +26,22 @@ class MovieComposer extends Composer {
         super(name);
         this.movie = movie;
     }
+
+    showMovie() {
+        console.log('Wooooow!');
+    }
+
+    playMusic() {
+        super.playMusic();
+        this.showMovie();
+    }
 }
 
 
 const c1 = new Composer('Chopin');
-console.log(Composer.getInstrument());
+console.log(c1.constructor.getInstrument());
+const c2 = new MovieComposer('Einaudi');
+c2.playMusic();
 
 console.log(c1 instanceof Composer);
 
